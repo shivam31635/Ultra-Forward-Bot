26cab26531dabcdfa06c2815ed28d606285b1b1b
crazyDeveloperz <133535122+crazyDeveloperz@users.noreply.github.com>
2024-12-21 23:20:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,18 @@
 import datetime
 from os import environ 
 
-#Dont Remove My Credit @Silicon_Bot_Update 
-#This Repo Is By @Silicon_Official 
-# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
-
 class Config:
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    API_ID = environ.get("API_ID", "27499182")
+    API_HASH = environ.get("API_HASH", "9c58142ef6abed28808a50e3e983c39c")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7122995544:AAFna-1AodMQtrliwyX8kyS8jsd8chk1434") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
-    DATABASE_URI = environ.get("DATABASE", "")
-    DATABASE_NAME = environ.get("DATABASE_NAME", "")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://forwradbot:forwradbot@cluster0.a9picjp.mongodb.net/?retryWrites=true&w=majority")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "Telegram_files")
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6133992240').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001876507111'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1001688312074") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
-    
-#Dont Remove My Credit @Silicon_Bot_Update 
-#This Repo Is By @Silicon_Official 
-# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
-
    
 class temp(object): 
     lock = {}

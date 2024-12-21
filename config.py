c4c7284ff3df2148bd579d4242e9d16f2677e4a6
Crazy Lover <122443634+shivam31635@users.noreply.github.com>
2024-12-22 00:59:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from os import environ
 class Config:
     API_ID = environ.get("API_ID", "27499182")
     API_HASH = environ.get("API_HASH", "9c58142ef6abed28808a50e3e983c39c")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7122995544:AAFna-1AodMQtrliwyX8kyS8jsd8chk1434") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7969829411:AAHSfw299OHm1R-0_aZLBfhtx6APWJLOnOs") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://Ultaforward:Ultaforward@cluster0.sjxy7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Telegram_files")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6133992240').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001876507111'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1001688312074") 
-    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/Crazybotz") 
+    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
    
 class temp(object): 

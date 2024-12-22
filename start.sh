0b67cfa7f0fc2b6828e3c0e6f7c550ea32c0747d
Crazy Lover <122443634+shivam31635@users.noreply.github.com>
2024-12-22 15:32:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Silicon-Developer/Ultra-Forward-Bot.git /TOM-BOT
+  git clone https://github.com/shivam31635/Ultra-Forward-Bot.git /TOM-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /TOM-BOT

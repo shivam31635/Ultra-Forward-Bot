08a29bd571892179da688de306d28dc4dd98dab6
Crazy Lover <122443634+shivam31635@users.noreply.github.com>
2024-12-22 15:53:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     DATABASE_NAME = environ.get("DATABASE_NAME", "Telegram_files")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6133992240').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001876507111'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/Crazybotz") 
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1001688312074") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
    

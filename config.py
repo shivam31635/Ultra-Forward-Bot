8ffaf1323ad0aac769b775a4665fc32eb6cc318f
Crazy Lover <122443634+shivam31635@users.noreply.github.com>
2024-12-22 16:29:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://Ultaforward:Ultaforward@cluster0.sjxy7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Telegram_files")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6133992240').split()]
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6249148586').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001876507111'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1001688312074") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
